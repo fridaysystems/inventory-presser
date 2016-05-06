8986534c4837fd8a1d3d47079147daee97856f62
unknown <JNorton@WEBDEV2.fridaysystems.local>
2016-05-06 11:06:40 -0400

    Added basic setting page for _dealer theme, may combine this with other settings at a later date.

--- inventory-presser.php
+++ inventory-presser.php
@@ -15,6 +15,7 @@ defined( 'ABSPATH' ) OR exit;
 $inventory_presser_include_paths = array(
 	'includes/class-add-custom-fields-to-search.php',
 	'includes/class-customize-admin-dashboard.php',
+	'includes/class-dealership-options.php', // This handles a couple options for _dealer theme, will likely combine this with import options
 	'includes/class-inventory-presser-vehicle.php',
 	'includes/class-modify-imports.php',
 	'includes/class-option-manager.php',
