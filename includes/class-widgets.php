7c5f9e8ec097e56c7734f8c2b543e7e8e14bb8e7
unknown <JNorton@WEBDEV2.fridaysystems.local>
2016-05-18 12:52:13 -0400

    Added stock photo slider widget.  Not yet in it's final form

--- includes/class-widgets.php
+++ includes/class-widgets.php
@@ -685,6 +685,115 @@ class KBB_Widget extends WP_Widget {
 
 } // Class KBB_Widget
 
+// Stock Photo Slider
+class Stock_Photo_Slider extends WP_Widget {
+
+	var $images = array(
+		'default' => array('text'=>'Bordered Rectangle', 'img'=>'kelley-blue-book.jpg'),
+		);
+
+	function __construct() {
+		parent::__construct(
+			'_invp_sps',
+			'Stock Photo Slider', 
+			array( 'description' => 'KBB image with link to kbb.com', ) 
+		);
+	}
+
+	// front-end
+	public function widget( $args, $instance ) {
+
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		// before and after widget arguments are defined by themes
+		echo $args['before_widget'];
+		if (!empty( $title ))
+		echo $args['before_title'] . $title . $args['after_title'];
+
+		?>
+
+<div class="flexslider flex-native">
+<ul class="slides">
+<li><?php echo sprintf('<img src="%s">',plugins_url( '/assets/sliders/german/slider-1.jpg', dirname(__FILE__))); ?></li>
+<li><?php echo sprintf('<img src="%s">',plugins_url( '/assets/sliders/german/slider-2.jpg', dirname(__FILE__))); ?></li>
+<li><?php echo sprintf('<img src="%s">',plugins_url( '/assets/sliders/german/slider-3.jpg', dirname(__FILE__))); ?></li>
+<li><?php echo sprintf('<img src="%s">',plugins_url( '/assets/sliders/german/slider-4.jpg', dirname(__FILE__))); ?></li>
+<li><?php echo sprintf('<img src="%s">',plugins_url( '/assets/sliders/german/slider-5.jpg', dirname(__FILE__))); ?></li>
+<li><?php echo sprintf('<img src="%s">',plugins_url( '/assets/sliders/german/slider-6.jpg', dirname(__FILE__))); ?></li>
+<li><?php echo sprintf('<img src="%s">',plugins_url( '/assets/sliders/german/slider-7.jpg', dirname(__FILE__))); ?></li>
+<li><?php echo sprintf('<img src="%s">',plugins_url( '/assets/sliders/german/slider-8.jpg', dirname(__FILE__))); ?></li>
+</ul>
+</div>
+
+		<?php
+
+		/*$image_keys = array_keys($this->images);
+		$image = (in_array($instance['image'], $image_keys)) ? $instance['image'] : $image_keys[0];
+
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		// before and after widget arguments are defined by themes
+		echo $args['before_widget'];
+		if (!empty( $title ))
+		echo $args['before_title'] . $title . $args['after_title'];
+
+		echo wpautop($instance['before_image']);
+		echo sprintf('<a href="%s" target="_blank"><img src="%s"></a>','http://kbb.com',plugins_url( '/assets/'.$this->images[$image]['img'], dirname(__FILE__)));
+		echo wpautop($instance['after_image']);*/
+		
+		echo $args['after_widget'];
+	}
+			
+	// Widget Backend 
+	public function form( $instance ) {
+
+		$image_keys = array_keys($this->images);
+
+		$title = isset($instance[ 'title' ]) ? $instance[ 'title' ] : '';
+		$before_image = isset($instance[ 'before_image' ]) ? $instance[ 'before_image' ] : '';
+		$image = isset($instance[ 'image' ]) ? $instance[ 'image' ] : $image_keys[0];
+		$after_image = isset($instance[ 'after_image' ]) ? $instance[ 'after_image' ] : '';
+
+		// Widget admin form
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'before_image' ); ?>"><?php _e( 'Text before image:' ); ?></label> 
+		<textarea class="widefat" id="<?php echo $this->get_field_id('before_image'); ?>" name="<?php echo $this->get_field_name('before_image'); ?>"><?php echo esc_attr( $before_image ); ?></textarea>
+		</p>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'image' ); ?>"><?php _e( 'Image:' ); ?></label> 
+
+		<select class="widefat" id="<?php echo $this->get_field_id('image'); ?>" name="<?php echo $this->get_field_name('image'); ?>">
+		<?php foreach ($this->images as $key => $imginfo) {
+			$select_text = ($key == $image) ? ' selected' : '';
+			echo sprintf('<option value="%s"%s>%s</option>',$key,$select_text,$imginfo['text']);
+		} ?>
+		</select>
+
+		</p>
+
+		<p>
+		<label for="<?php echo $this->get_field_id( 'after_image' ); ?>"><?php _e( 'Text after image:' ); ?></label> 
+		<textarea class="widefat" id="<?php echo $this->get_field_id('after_image'); ?>" name="<?php echo $this->get_field_name('after_image'); ?>"><?php echo esc_attr( $after_image ); ?></textarea>
+		</p>
+		<?php
+	}
+		
+	// Updating widget replacing old instances with new
+	public function update( $new_instance, $old_instance ) {
+		$image_keys = array_keys($this->images);
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['before_image'] = ( ! empty( $new_instance['before_image'] ) ) ? strip_tags( $new_instance['before_image'] ) : '';
+		$instance['image'] = ( ! empty( $new_instance['image'] ) ) ? strip_tags( $new_instance['image'] ) : $image_keys[0];
+		$instance['after_image'] = ( ! empty( $new_instance['after_image'] ) ) ? strip_tags( $new_instance['after_image'] ) : '';
+		return $instance;
+	}
+
+} // Class Stock_Photo_Slider
+
 // bootstrap class for these widgets
 class Inventory_Presser_Location_Widgets {
 
@@ -699,6 +808,7 @@ class Inventory_Presser_Location_Widgets {
 		register_widget('Inventory_Presser_Location_Phones');
 		register_widget('Carfax_Widget');
 		register_widget('KBB_Widget');
+		register_widget('Stock_Photo_Slider');
 	}
 
 	function check_ids() {
