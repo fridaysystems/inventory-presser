d8fa0927a572f3dc2e3c762e80beb8ebfd924c78
Corey Salzano <email@coreysalzano.com>
2017-09-13 16:19:07 -0400

    Add taxonomy term names and slugs to searches, so "suv" and "diesel" searches are possible

--- includes/class-add-custom-fields-to-search.php
+++ includes/class-add-custom-fields-to-search.php
@@ -33,7 +33,14 @@ if ( ! class_exists( 'Add_Custom_Fields_To_Search' ) ) {
 		    global $wpdb;
 
 		    if ( is_search() ) {
+
+		    	//join to search post meta values like year, make, model, trim, etc
 		        $join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
+
+		        //these joins are so taxonomy terms like Sport Utility Vehicle => suv are included
+		        $join .= 'LEFT JOIN ' . $wpdb->term_relationships . ' tr ON ' . $wpdb->posts . '.ID = tr.object_id '
+		        	. 'INNER JOIN ' . $wpdb->term_taxonomy . ' tt ON tt.term_taxonomy_id = tr.term_taxonomy_id '
+		        	. 'INNER JOIN ' . $wpdb->terms . ' t ON t.term_id = tt.term_id ';
 		    }
 
 		    return $join;
@@ -49,8 +56,10 @@ if ( ! class_exists( 'Add_Custom_Fields_To_Search' ) ) {
 
 		    if ( is_search() ) {
 		        $where = preg_replace(
-		            "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
-		            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+		            "/\(\s*" . $wpdb->posts . ".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+		            "(" . $wpdb->posts . ".post_title LIKE $1) OR (" . $wpdb->postmeta . ".meta_value LIKE $1) OR (t.name LIKE $1) OR (t.slug LIKE $1)",
+		             $where
+		        );
 		    }
 
 		    return $where;
