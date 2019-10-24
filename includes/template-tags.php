425459554f6b3ee7d3e41b2a394c53c6bb03fd1c
Corey Salzano <corey.salzano@gmail.com>
2019-10-24 11:02:23 -0400

    Change to whitespace only

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -12,11 +12,13 @@ defined( 'ABSPATH' ) or exit;
  * @author     Corey Salzano <corey@friday.systems>
  */
 
-function invp_get_the_vin() {
+function invp_get_the_vin()
+{
 	return get_post_meta( get_the_ID(), apply_filters( 'invp_prefix_meta_key', 'vin' ), true );
 }
 
-function invp_get_the_price() {
+function invp_get_the_price()
+{
 	$vehicle = new Inventory_Presser_Vehicle( get_the_ID() );
 	return $vehicle->price();
 }
