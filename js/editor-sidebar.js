4e23c7cdfe809c477adc7e3be96bf34c0e7dd019
Corey Salzano <corey.salzano@gmail.com>
2020-01-13 12:42:25 -0500

    Adds stock number and model year to the Editor sidebar

--- js/editor-sidebar.js
+++ js/editor-sidebar.js
@@ -3,9 +3,39 @@
 	var PluginSidebar = wp.editPost.PluginSidebar;
 	var el = wp.element.createElement;
 	var Text = wp.components.TextControl;
+	var Select = wp.components.SelectControl;
 	var withSelect = wp.data.withSelect;
 	var withDispatch = wp.data.withDispatch;
 	var compose = wp.compose.compose;
+
+	function getLabel( meta_key )
+	{
+		switch( meta_key.substring( 18 ) )
+		{
+			case 'year':
+				return 'Year';
+
+			case 'stock_number':
+				return 'Stock Number';
+
+			case 'vin':
+				return 'VIN';
+		}
+		return meta_key;
+	}
+
+	function yearOptions()
+	{
+		var options = [];
+		for(var y=new Date().getFullYear(); y>1919; y-- )
+		{
+			options.push({
+				label: y,
+				value: y
+			});
+		}
+		return options;
+	}
 
 	var MetaBlockField = compose(
 		withDispatch( function( dispatch, props ) {
@@ -26,8 +56,37 @@
 		} )
 	)( function( props ) {
 		return el( Text, {
-			label: 'VIN',
+			label: getLabel( props.fieldName ),
 			value: props.metaFieldValue,
+			onChange: function( content ) {
+				props.setMetaFieldValue( content );
+			},
+		} );
+	} );
+
+	//Drop down for model year
+	var MetaBlockFieldSelect = compose(
+		withDispatch( function( dispatch, props ) {
+			return {
+				setMetaFieldValue: function( value ) {
+					dispatch( 'core/editor' ).editPost(
+						{ meta: { [ props.fieldName ]: value } }
+					);
+				}
+			}
+		} ),
+		withSelect( function( select, props ) {
+			return {
+				metaFieldValue: select( 'core/editor' )
+					.getEditedPostAttribute( 'meta' )
+					[ props.fieldName ],
+			}
+		} )
+	)( function( props ) {
+		return el( Select, {
+			label: getLabel( props.fieldName ),
+			value: props.metaFieldValue,
+			options: yearOptions(),
 			onChange: function( content ) {
 				props.setMetaFieldValue( content );
 			},
@@ -46,8 +105,14 @@
 					{ className: 'invp-editor-sidebar' },
 					el( MetaBlockField,
 						{ fieldName: 'inventory_presser_vin' }
+					),
+					el( MetaBlockField,
+						{ fieldName: 'inventory_presser_stock_number' }
+					),
+					el( MetaBlockFieldSelect,
+						{ fieldName: 'inventory_presser_year' }
 					)
-				)
+				),
 			);
 		}
 	} );
