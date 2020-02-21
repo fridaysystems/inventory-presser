df4f814e12a89c22d7cf8cb7aed36aef5ed96806
Corey Salzano <corey.salzano@gmail.com>
2020-02-21 09:57:35 -0500

    Rebind a select element onchange handler after we add a new one to the options page for this additional listing pages feature

--- js/page-settings.js
+++ js/page-settings.js
@@ -9,6 +9,9 @@
 		$('button#add_additional_listings_page').on('click', function(){
 			var row_html = $('table.additional_listings_pages tbody tr:last')[0].outerHTML;
 			$('table.additional_listings_pages tbody').append( repeat_row( row_html ) );
+			//rebind the operator selects
+			$('select.operator').off('change');
+			$('select.operator').on('change', toggle_value_input );
 			return false;
 		});
 
@@ -56,15 +59,6 @@
 		tr_html = tr_html.replace( re, 'value=""' );
 
 		return tr_html;
-
-		/*
-			<tr>
-				<td style="width: 100%;">http://localhost:8080/sandbox/<input type="text" id="additional_listings_pages_slug_0" name="inventory_presser[additional_listings_pages][0][url_path]" value="cash-deals"></td>
-				<td><select id="additional_listings_pages_key_0" name="inventory_presser[additional_listings_pages][0][key]"><option value="beam">Beam</option><option value="body_style">Body style</option><option value="car_id">Car id</option><option value="carfax_have_report">Carfax have report</option><option value="carfax_one_owner">Carfax one owner</option><option value="carfax_url_icon">Carfax url icon</option><option value="carfax_url_report">Carfax url report</option><option value="color">Color</option><option value="dealer_id">Dealer id</option><option value="down_payment" selected="selected">Down payment</option><option value="edmunds_style_id">Edmunds style id</option><option value="engine">Engine</option><option value="featured">Featured</option><option value="hull_material">Hull material</option><option value="interior_color">Interior color</option><option value="last_modified">Last modified</option><option value="leads_id">Leads id</option><option value="length">Length</option><option value="make">Make</option><option value="model">Model</option><option value="msrp">Msrp</option><option value="odometer">Odometer</option><option value="payment">Payment</option><option value="payment_frequency">Payment frequency</option><option value="price">Price</option><option value="stock_number">Stock number</option><option value="title_status">Title status</option><option value="transmission_speeds">Transmission speeds</option><option value="trim">Trim</option><option value="vin">Vin</option><option value="year">Year</option><option value="youtube">Youtube</option></select></td>
-				<td><select id="additional_listings_pages_operator_0" name="inventory_presser[additional_listings_pages][0][operator]" class="operator"><option value="exists">exists</option><option value="does_not_exist">does not exist</option><option value="greater_than">greater than</option><option value="less_than">less than</option><option value="equal_to">equal to</option><option value="not_equal_to">not equal to</option></select></td>
-				<td><input type="text" id="additional_listings_pages_value_0" name="inventory_presser[additional_listings_pages][0][value]" value=""></td>
-			</tr>
-		*/
 	}
 
 	function toggle_value_input()
