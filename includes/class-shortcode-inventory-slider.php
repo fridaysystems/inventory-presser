7d66d961f37a9d97b144b331f6af2bf41a919cc0
Corey Salzano <corey.salzano@gmail.com>
2020-12-04 09:07:55 -0500

    Rewrites the Vehicle Slider Widget so that the vehicle IDs that will be included in the slideshow are determined inside their own static method. This method also allows other pieces of code to obtain the list of IDs without creating the widget. _dealer_valerie theme needs this to mesh the slider photos with other media attachments.

--- includes/class-shortcode-inventory-slider.php
+++ includes/class-shortcode-inventory-slider.php
@@ -43,22 +43,54 @@ class Inventory_Presser_Shortcode_Slider
 	 */
 	function content( $atts ) {
 
-		//Need flexslider for this content
-		wp_enqueue_script( 'invp-flexslider' );
-		wp_enqueue_style( 'invp-flexslider' );
-
-		// process shortcode attributes
+		//Canvass shortcode attributes
 		$atts = shortcode_atts( array(
 			'per_page' => 10,
 			'captions' => 'true',
 			'orderby'  => 'rand',
 			'order'    => 'ASC',
-		), $atts, 'inventory_slider' );
-
-		//Use shortcode_atts_inventory_slider to filter the incoming attributes
-
+		), $atts, 'inventory_slider' ); //Use shortcode_atts_inventory_slider to filter the incoming attributes
 		$atts['captions'] = 'true' === $atts['captions'];
 
+		//Get the vehicle IDs and loop over them
+		$inventory_ids = self::get_vehicle_IDs( $atts );
+		if( empty( $inventory_ids ) )
+		{
+			return '';
+		}
+
+		//Need flexslider for this content
+		wp_enqueue_script( 'invp-flexslider' );
+		wp_enqueue_style( 'invp-flexslider' );
+
+		$flex_html = '<div class="flexslider flex-native">'
+			. '<ul class="slides">';
+
+		foreach( $inventory_ids as $inventory_id )
+		{
+			$flex_html .= sprintf(
+				'<li><a class="flex-link" href="%s">'
+				. '<div class="grid-image" style="background-image: url(\'%s\');">'
+				. '</div>',
+				get_the_permalink( $inventory_id ),
+				invp_get_the_photo_url( 'large', $inventory_id )
+			);
+
+			if( $atts['captions'] ) {
+				$flex_html .= sprintf(
+					'<p class="flex-caption">%s</p>',
+					get_the_title( $inventory_id )
+				);
+			}
+
+			$flex_html .= '</a></li>';
+		}
+
+		return $flex_html . '</ul></div>';
+	}
+
+	public static function get_vehicle_IDs( $shortcode_atts )
+	{
 		$gpargs = array(
 			'posts_per_page' => 10,
 			'post_type'   => INVP::POST_TYPE,
@@ -75,8 +107,8 @@ class Inventory_Presser_Shortcode_Slider
 				)
 			),
 			'fields'  => 'ids',
-			'orderby' => $atts['orderby'],
-			'order'   => $atts['order'],
+			'orderby' => $shortcode_atts['orderby'],
+			'order'   => $shortcode_atts['order'],
 		);
 
 		$inventory_ids = get_posts($gpargs);
@@ -106,42 +138,19 @@ class Inventory_Presser_Shortcode_Slider
 					)
 				),
 				'fields'  => 'ids',
-				'orderby' => $atts['orderby'],
-				'order'   => $atts['order'],
+				'orderby' => $shortcode_atts['orderby'],
+				'order'   => $shortcode_atts['order'],
 			);
 
 			$inventory_ids += get_posts($gpargs);
 		}
 
 		if( ! $inventory_ids ) {
-			return '';
+			return [];
 		}
 
 		shuffle( $inventory_ids );
 
-		$flex_html = '<div class="flexslider flex-native">'
-			. '<ul class="slides">';
-
-		foreach( $inventory_ids as $inventory_id )
-		{
-			$flex_html .= sprintf(
-				'<li><a class="flex-link" href="%s">'
-				. '<div class="grid-image" style="background-image: url(\'%s\');">'
-				. '</div>',
-				get_the_permalink( $inventory_id ),
-				invp_get_the_photo_url( 'large', $inventory_id )
-			);
-
-			if( $atts['captions'] ) {
-				$flex_html .= sprintf(
-					'<p class="flex-caption">%s</p>',
-					get_the_title( $inventory_id )
-				);
-			}
-
-			$flex_html .= '</a></li>';
-		}
-
-		return $flex_html . '</ul></div>';
+		return $inventory_ids;
 	}
 }
