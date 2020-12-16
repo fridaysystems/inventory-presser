df5d88bf8bcf09b28eca516d3d512b7c8b5dd0dc
Corey Salzano <corey.salzano@gmail.com>
2020-12-16 14:11:29 -0500

    Changes the [iframe] shortcode to throw a notice about it being deprecated and replaced with [invp_iframe].

--- includes/class-shortcode-iframe.php
+++ includes/class-shortcode-iframe.php
@@ -19,7 +19,9 @@ class Inventory_Presser_Shortcode_Iframe
 	 */
 	function add()
 	{
-		add_shortcode( 'iframe', array( $this, 'content') );
+		add_shortcode( 'iframe', array( $this, 'content_but_wrong') );
+		add_shortcode( 'invp-iframe', array( $this, 'content') );
+		add_shortcode( 'invp_iframe', array( $this, 'content') );
 	}
 
 	/**
@@ -32,6 +34,16 @@ class Inventory_Presser_Shortcode_Iframe
 	function hooks()
 	{
 		add_action( 'init', array( $this, 'add' ) );
+	}
+
+	function content_but_wrong( $atts )
+	{
+		_doing_it_wrong(
+			__FUNCTION__,
+			__( 'The [iframe] shortcode was deprecated in version 12.2.0. Use [invp_iframe] instead. See https://inventorypresser.com/docs/shortcodes/', 'inventory-presser' ),
+			'12.2.0'
+		);
+		return $this->content( $atts );
 	}
 
 	/**
