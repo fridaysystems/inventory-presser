9dc0321621df49899e6a535a9a94d58672aa8aea
Corey Salzano <corey.salzano@gmail.com>
2021-01-07 11:44:53 -0500

    Adds hooks and a callback method to the new badges class for NextGear Vehicle Inspection links using a simple button that will appear similar to the View Details button on archives now. I am not aware of a NextGear-provided button for these links.

--- includes/class-badges.php
+++ includes/class-badges.php
@@ -18,7 +18,7 @@ class Inventory_Presser_Badges
 	 *
 	 * @return void
 	 */
-	function add_carfax()
+	public function add_carfax()
 	{
 		$carfax_html = invp_get_the_carfax_icon_html();
 		if( '' != $carfax_html )
@@ -26,6 +26,24 @@ class Inventory_Presser_Badges
 			?><div class="carfax-wrapper"><?php
 				echo $carfax_html;
 			?></div><?php
+		}
+	}
+
+	public function add_nextgear()
+	{
+		$url = INVP::get_meta( 'nextgear_inspection_url' );
+		if( ! empty( $url ) )
+		{
+			//CSS classes on the <a>
+			$classes = apply_filters( 'invp_button_classes_nextgear_inspection_url', 'wp-block-button__link button' );
+
+			?><div class="nextgear-wrapper">
+				<a class="<?php echo $classes; ?>" href="<?php echo $url; ?>"><?php
+
+				echo apply_filters( 'invp_button_text_nextgear_inspection_url', __( 'See Digital Inspection Report', 'inventory-presser' ) );
+
+			?></a>
+			</div><?php
 		}
 	}
 
@@ -38,5 +56,9 @@ class Inventory_Presser_Badges
 			add_action( 'invp_archive_buttons', array( $this, 'add_carfax' ) );
 			add_action( 'invp_single_buttons',  array( $this, 'add_carfax' ) );
 		}
+
+		//NextGear vehicle inspections
+		add_action( 'invp_archive_buttons', array( $this, 'add_nextgear' ) );
+		add_action( 'invp_single_buttons', array( $this, 'add_nextgear' ) );
 	}
 }
