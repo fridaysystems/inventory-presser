20d6c680f3ca026b3b3d3a317333b9374a0a2ff3
Corey Salzano <corey.salzano@gmail.com>
2021-10-14 16:57:15 -0400

    Upgrades the add-on framework to accomodate add-ons where the license key is stored using a key name other than license_key in the option. Perhaps with an underscore at the front like _license_key.

--- includes/class-addon.php
+++ includes/class-addon.php
@@ -45,9 +45,9 @@ if( ! class_exists( 'Inventory_Presser_Addon' ) )
 			) );
 		}
 
-		public static function add_license_key_box( $option_name, $settings_section, $sanitize_values_hook_name )
+		public static function add_license_key_box( $option_name, $settings_section, $sanitize_values_hook_name, $license_key_key_name = 'license_key' )
 		{
-			add_action( 'admin_init', function() use ( $option_name, $settings_section ) {
+			add_action( 'admin_init', function() use ( $option_name, $settings_section, $license_key_key_name ) {
 
 				//is $settings_section an existing section?
 				global $wp_settings_sections;
@@ -58,21 +58,22 @@ if( ! class_exists( 'Inventory_Presser_Addon' ) )
 				}
 
 				$option = get_option( $option_name );
-				$current_value = empty( $option['license_key'] ) ? '' : $option['license_key'];
+				$current_value = empty( $option[$license_key_key_name] ) ? '' : $option[$license_key_key_name];
 
 				add_settings_field(
-					'license_key', // id
+					$license_key_key_name, // id
 					__( 'License key', 'inventory-presser' ), // title
-					function() use ( $option_name, $current_value ) {
-						?><p><input type="text" name="<?php echo $option_name; ?>[license_key]" class="regular-text code" id="<?php echo $option_name; ?>[license_key]" value="<?php echo $current_value; ?>" /></p>
-						<p class="description"><?php printf( '%s <a href="https://inventorypresser.com/">https://inventorypresser.com/</a> %s', __( 'Obtain a key at', 'inventory-presser' ), __( 'to receive plugin updates.', 'inventory-presser-divert-cf7' ) ); ?></p><?php
+					function() use ( $option_name, $current_value, $license_key_key_name ) {
+						?><p><input type="text" name="<?php echo $option_name; ?>[<?php echo $license_key_key_name; ?>]" class="regular-text code" id="<?php echo $option_name; ?>[<?php echo $license_key_key_name; ?>]" value="<?php echo $current_value; ?>" /></p>
+						<p class="description"><?php printf( '%s <a href="https://inventorypresser.com/">https://inventorypresser.com/</a> %s', __( 'Obtain a key at', 'inventory-presser' ), __( 'to receive plugin updates.', 'inventory-presser' ) ); ?></p><?php
 					}, // callback
 					INVP::option_page(), // page
 					$settings_section // section
 				);
 			}, 20 );
 
-			add_filter( $sanitize_values_hook_name, array( __CLASS__, 'sanitize_and_activate_license_key' ), 10, 3 );
+			$hook_parameters_count = 'license_key' == $license_key_key_name ? 3 : 4;
+			add_filter( $sanitize_values_hook_name, array( __CLASS__, 'sanitize_and_activate_license_key' ), 10, $hook_parameters_count );
 		}
 
 		public static function make_sure_license_is_activated( $license_item, $license_key )
@@ -84,12 +85,12 @@ if( ! class_exists( 'Inventory_Presser_Addon' ) )
 			if( ! $license->is_active() ) { $license->activate(); }
 		}
 		
-		public static function sanitize_and_activate_license_key( $sanitized, $input, $license_item )
+		public static function sanitize_and_activate_license_key( $sanitized, $input, $license_item, $license_key_key_name = 'license_key' )
 		{				
-			if ( isset( $input['license_key'] ) )
+			if ( isset( $input[$license_key_key_name] ) )
 			{
-				$sanitized['license_key'] = sanitize_text_field( $input['license_key'] );
-				self::make_sure_license_is_activated( $license_item, $sanitized['license_key'] );
+				$sanitized[$license_key_key_name] = sanitize_text_field( $input[$license_key_key_name] );
+				self::make_sure_license_is_activated( $license_item, $sanitized[$license_key_key_name] );
 			}
 			return $sanitized;
 		}
