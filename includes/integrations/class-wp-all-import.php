d475a65942002576e28b14acbb85d68d6be01196
Corey Salzano <corey.salzano@gmail.com>
2022-06-17 00:15:05 -0400

    Marks vehicles for sale in the availabilities taxonomy when they are updated by WP All Import unless they already have a term relationship in the taxonomy.

--- includes/integrations/class-wp-all-import.php
+++ includes/integrations/class-wp-all-import.php
@@ -8,7 +8,16 @@ defined( 'ABSPATH' ) or exit;
  * saving them as individual options.
  */
 class Inventory_Presser_WP_All_Import
-{	
+{
+	public function add_hooks()
+	{
+		//Help save options in a multi-valued meta field
+		add_action( 'pmxi_update_post_meta', array( $this, 'detect_piped_options' ), 10, 3 );
+
+		//Mark imported vehicles as "For Sale" in the Availability taxonomy
+		add_action( 'pmxi_saved_post', array( $this, 'set_availability_for_sale' ), 10, 3 );
+	}
+
 	/**
 	 * detect_piped_options
 	 * 
@@ -48,9 +57,34 @@ class Inventory_Presser_WP_All_Import
 			add_post_meta( $post_id, $meta_key, $option );
 		}
 	}
-
-	public function add_hooks()
+	
+	/**
+	 * set_availability_for_sale
+	 * 
+	 * Not all feed imports have a for sale/sold bit that matches up nicely with
+	 * our Availability taxonomy. Mark vehicles as for sale if no relationship
+	 * exists in the taxonomy.
+	 *
+	 * @param  int $post_id
+	 * @param  SimpleXMLElement $xml_node
+	 * @param  bool $is_update
+	 * @return void
+	 */
+	public function set_availability_for_sale( $post_id, $xml_node, $is_update )
 	{
-		add_action( 'pmxi_update_post_meta', array( $this, 'detect_piped_options' ), 10, 3 );
+		$taxonomy = 'availability';
+		if( ! empty( wp_get_object_terms( $post_id, $taxonomy ) ) )
+		{
+			//There is already a relationship
+			return;
+		}
+
+		//Do we have a For Sale term?
+		$term = get_term_by( 'slug', 'for-sale', $taxonomy );
+		if( $term )
+		{
+			//Yes, create the relationship
+			wp_set_object_terms( $post_id, $term->term_id, $taxonomy );
+		}
 	}
 }
