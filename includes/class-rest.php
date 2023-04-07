30603aa59c88cc3241885f8de57cf2731ab085e0
Corey Salzano <corey.salzano@gmail.com>
2023-04-07 09:59:36 -0400

    Converts spaces to tabs in the REST class. Adds a feature that allows REST queries for attachments to provide the meta key "inventory_presser_photo_number" as the orderby parameter. Enables photos to be requested in sequence via the REST API.

--- includes/class-rest.php
+++ includes/class-rest.php
@@ -7,49 +7,85 @@ defined( 'ABSPATH' ) || exit;
  * Adds routes to the REST API at /wp-json/invp/
  */
 class Inventory_Presser_REST {    
-    /**
-     * add_hooks
-     *
-     * @return void
-     */
-    public function add_hooks() {
-        add_action('rest_api_init', array( $this, 'add_route' ));
-    }
-    
-    /**
-     * add_route
-     *
-     * @return void
-     */
-    public function add_route() {
-        register_rest_route(
-            'invp/v1', '/settings/', array(
-            'methods'             => 'GET',
-            'callback'            => array( $this, 'response' ),
-            'permission_callback' => '__return_true',
-            ) 
-        );
-    }
-    
-    /**
-     * response
-     *
-     * @return void
-     */
-    public function response() {
-        /**
-         * This is controversial. Some people think options table data will 
-         * never be public. Only allow these keys:
-         */
+	/**
+	 * add_hooks
+	 *
+	 * @return void
+	 */
+	public function add_hooks() {
+		// Adds a /invp/v1/settings route.
+		add_action('rest_api_init', array( $this, 'add_route' ));
 
-        $public_keys = array(
-       		'use_carfax',
-        );
+		// Allow attachments to be ordered by the inventory_presser_photo_number meta value.
+		add_filter( 'rest_attachment_collection_params', array( $this, 'allow_orderby_photo_number' ) );
+		add_filter( 'rest_attachment_query', array( $this, 'orderby_photo_number' ), 10, 2 );
+	}
 
-        return array_filter(
-            INVP::settings(), function ( $k ) use ( $public_keys ) {
-                return in_array($k, $public_keys); 
-            }, ARRAY_FILTER_USE_KEY
-        );
-    }
+	/**
+	 * Adds the photo_number meta field to the allowed orderby values.
+	 *
+	 * @param  array $params
+	 * @return array
+	 */
+	public function allow_orderby_photo_number( $params ) {
+		$params['orderby']['enum'][] = apply_filters( 'invp_prefix_meta_key', 'photo_number' );
+		return $params;
+	}
+
+	/**
+	 * Changes the query args for requests to order attachments by the
+	 * photo_number meta key.
+	 *
+	 * @param  array $args Array of arguments for WP_Query.
+	 * @param  WP_REST_Request $request The REST API request.
+	 * @return array
+	 */
+	public function orderby_photo_number( $args, $request ) {
+		$order_by = $request->get_param( 'orderby' );
+		if ( isset( $order_by ) && apply_filters( 'invp_prefix_meta_key', 'photo_number' ) === $order_by ) {
+			$args['meta_key'] = $order_by;
+			$args['orderby']  = 'meta_value_num'; // user 'meta_value_num' for numerical fields
+		}
+		return $args;
+	}
+
+	public function order_attachments( $params ) {
+		// Is the parent of this attachment a vehicle?
+		$params['orderby']['enum'][] = apply_filters( 'invp_prefix_meta_key', 'photo_number' );
+		return $params;
+	}
+
+	/**
+	 * Adds a /invp/v1/settings route.
+	 *
+	 * @return void
+	 */
+	public function add_route() {
+		register_rest_route(
+			'invp/v1',
+			'/settings/',
+			array(
+				'methods'             => 'GET',
+				'callback'            => array( $this, 'response' ),
+				'permission_callback' => '__return_true',
+			)
+		);
+	}
+
+	/**
+	 * response
+	 *
+	 * @return void
+	 */
+	public function response() {
+		$public_keys = array(
+			'use_carfax',
+		);
+
+		return array_filter(
+			INVP::settings(), function ( $k ) use ( $public_keys ) {
+				return in_array( $k, $public_keys );
+			}, ARRAY_FILTER_USE_KEY
+		);
+	}
 }
