e641a63a71210a59ddc4d91a9825ae50dd09ab98
Corey Salzano <938411+csalzano@users.noreply.github.com>
2023-10-09 13:51:13 -0400

    Ran npm run build after the package updates

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-element'), 'version' => 'abb0101d9c8a8c6e7980');
+<?php return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-element'), 'version' => '3830987939ac5fdbc286');
