393f08c7334b58699ba063b27ecc0fcf8992d2c3
Corey Salzano <938411+csalzano@users.noreply.github.com>
2024-01-04 10:45:59 -0500

    Bug fix. Stops joining searches to the taxonomy terms tables. All vehicle taxonomy terms are mirrored in post meta, so this was no longer necessary. It was also breaking searches on inventorypresser.com. Also changes the search class to satisfy some of the coding standard issues shown in my editor.

--- includes/class-add-custom-fields-to-search.php
+++ includes/class-add-custom-fields-to-search.php
@@ -19,27 +19,22 @@ if ( ! class_exists( 'Add_Custom_Fields_To_Search' ) ) {
 
 
 		/**
-		 * hooks
-		 *
 		 * Adds hooks
 		 *
 		 * @return void
 		 */
 		public function add_hooks() {
-			add_filter( 'posts_distinct', array( $this, 'cf_search_distinct' ) );
 			add_filter( 'posts_join', array( $this, 'cf_search_join' ) );
 			add_filter( 'posts_where', array( $this, 'cf_search_where' ) );
 		}
 
 		/**
-		 * is_media_library
-		 *
 		 * Are we looking at the Media Library?
 		 *
 		 * @return bool
 		 */
-		function is_media_library() {
-			return 'upload.php' == basename( $_SERVER['REQUEST_URI'], '?' . $_SERVER['QUERY_STRING'] );
+		protected function is_media_library() {
+			return 'upload.php' === basename( $_SERVER['REQUEST_URI'], '?' . $_SERVER['QUERY_STRING'] );
 		}
 
 		/**
@@ -59,26 +54,18 @@ if ( ! class_exists( 'Add_Custom_Fields_To_Search' ) ) {
 
 			// join to search post meta values like year, make, model, trim, etc.
 			$join .= " LEFT JOIN $wpdb->postmeta searchmeta ON $wpdb->posts.ID = searchmeta.post_id ";
-
-			// these joins are so taxonomy terms like Sport Utility Vehicle => suv are included.
-			$join .= "LEFT JOIN $wpdb->term_relationships tr ON $wpdb->posts.ID = tr.object_id "
-				. "INNER JOIN $wpdb->term_taxonomy tt ON tt.term_taxonomy_id = tr.term_taxonomy_id "
-				. "INNER JOIN $wpdb->terms t ON t.term_id = tt.term_id ";
-
 			return $join;
 		}
 
 		/**
-		 * cf_search_where
-		 *
 		 * Modify the search query with posts_where
 		 *
 		 * @see http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_where
 		 *
 		 * @param  string $where
-		 * @return void
+		 * @return string
 		 */
-		function cf_search_where( $where ) {
+		public function cf_search_where( $where ) {
 
 			if ( ! is_search() || $this->is_media_library() ) {
 				return $where;
@@ -87,27 +74,9 @@ if ( ! class_exists( 'Add_Custom_Fields_To_Search' ) ) {
 			global $wpdb;
 			$where = preg_replace(
 				"/\(\s*$wpdb->posts.post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
-				"($wpdb->posts.post_title LIKE $1) OR ( searchmeta.meta_value LIKE $1) OR (t.name LIKE $1) OR (t.slug LIKE $1)",
+				"($wpdb->posts.post_title LIKE $1) OR ( searchmeta.meta_value LIKE $1 ) ",
 				$where
 			);
-
-			return $where;
-		}
-
-		/**
-		 * cf_search_distinct
-		 *
-		 * Prevent duplicates
-		 *
-		 * @see http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_distinct
-		 *
-		 * @param  string $where
-		 * @return void
-		 */
-		function cf_search_distinct( $where ) {
-			if ( is_search() ) {
-				return 'DISTINCT';
-			}
 
 			return $where;
 		}
