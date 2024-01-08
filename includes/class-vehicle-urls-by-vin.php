4ac5721935ee76f06f81f2f61ff1d0dd7443ded9
Corey Salzano <938411+csalzano@users.noreply.github.com>
2024-01-08 10:35:06 -0500

    Cleans up the vehicle VIN URLs class to adhere to the coding standard. Makes one small bug fix where the return boolean of wp_redirect() was passed into exit().

--- includes/class-vehicle-urls-by-vin.php
+++ includes/class-vehicle-urls-by-vin.php
@@ -1,29 +1,30 @@
 <?php
+/**
+ * Vehicle_URLs_By_VIN
+ *
+ * This class redirects URLs based on a VIN to proper permalinks
+ *
+ * Create a redirect that takes a URL like this:
+ * http://localhost/vin/JM1NB354940406328/ and redirects to the post that
+ * lives at some permalink like
+ * http://localhost/inventory/2004-mazda-speed-miata/
+ *
+ * @since      1.2.1
+ * @package inventory-presser
+ * @subpackage inventory-presser/includes
+ * @author     Corey Salzano <corey@friday.systems>, John Norton <norton@fridaynet.com>
+ */
+
 defined( 'ABSPATH' ) || exit;
 
 if ( ! class_exists( 'Vehicle_URLs_By_VIN' ) ) {
+
 	/**
 	 * Vehicle_URLs_By_VIN
-	 *
-	 * This class redirects URLs based on a VIN to proper permalinks
-	 *
-	 * Create a redirect that takes a URL like this:
-	 * http://localhost/vin/JM1NB354940406328/ and redirects to the post that
-	 * lives at some permalink like
-	 * http://localhost/inventory/2004-mazda-speed-miata/
-	 *
-	 * @since      1.2.1
-	 * @package inventory-presser
-	 * @subpackage inventory-presser/includes
-	 * @author     Corey Salzano <corey@friday.systems>, John Norton <norton@fridaynet.com>
 	 */
 	class Vehicle_URLs_By_VIN {
 
-
-
 		/**
-		 * hooks
-		 *
 		 * Adds hooks that power the URL redirects.
 		 *
 		 * @return void
@@ -35,55 +36,48 @@ if ( ! class_exists( 'Vehicle_URLs_By_VIN' ) ) {
 		}
 
 		/**
-		 * add_vin_rewrite
-		 *
-		 * Adds a rewrite rule to parse URLs like http://localhost/vin/JM1NB354940406328/
+		 * Adds a rewrite rule to redirect URLs like http://localhost/vin/JM1NB354940406328/
 		 *
 		 * @return void
 		 */
-		function add_vin_rewrite() {
-			// allow VINs as short as five digits because classic cars
+		public function add_vin_rewrite() {
+			// Allow VINs as short as five digits because classic cars.
 			add_rewrite_rule( '^vin/([A-Z0-9]{5,17})/?$', 'index.php?vin=$matches[1]', 'top' );
 		}
 
 		/**
-		 * add_query_vars
-		 *
 		 * Adds 'vin' to the list of recognized query variables.
 		 *
-		 * @param  array $vars An array of query variables
+		 * @param  array $vars An array of query variables.
 		 * @return array The expanded array of query variables
 		 */
-		function add_query_vars( $vars ) {
+		public function add_query_vars( $vars ) {
 			$vars[] = 'vin';
 			return $vars;
 		}
 
 		/**
-		 * redirect_vin_urls
-		 *
 		 * Redirects the request to a vehicle permalink if a 'vin' query
 		 * variable is present.
 		 *
 		 * @return void
 		 */
-		function redirect_vin_urls() {
+		public function redirect_vin_urls() {
 			$vin = get_query_var( 'vin' );
 			if ( $vin ) {
-				exit( wp_redirect( $this->find_vehicle_url( $vin ) ) );
+				wp_safe_redirect( $this->find_vehicle_url( $vin ) );
+				exit;
 			}
 		}
 
 		/**
-		 * find_vehicle_url
-		 *
 		 * Takes a VIN and finds its permalink. If the vehicle is not in
 		 * inventory, the vehicle archive URL is returned.
 		 *
-		 * @param  string $vin A vehicle identification number
+		 * @param  string $vin A vehicle identification number.
 		 * @return string A vehicle permalink or the link to the vehicle archive.
 		 */
-		function find_vehicle_url( $vin ) {
+		protected function find_vehicle_url( $vin ) {
 			$posts = get_posts(
 				array(
 					'post_type'   => INVP::POST_TYPE,
@@ -92,7 +86,7 @@ if ( ! class_exists( 'Vehicle_URLs_By_VIN' ) ) {
 					'meta_value'  => $vin,
 				)
 			);
-			if ( 1 === sizeof( $posts ) ) {
+			if ( 1 === count( $posts ) ) {
 				return get_permalink( $posts[0] );
 			}
 			return get_post_type_archive_link( INVP::POST_TYPE );
