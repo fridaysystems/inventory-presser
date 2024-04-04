e31d336dbdaa26c34657075f9d179bd866d1fdae
Corey Salzano <938411+csalzano@users.noreply.github.com>
2024-04-04 10:29:39 -0400

    Runs "npm update" and "npm run build"

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-element'), 'version' => '3830987939ac5fdbc286');
+<?php return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-element'), 'version' => '995f9b65150bffdd7c3c');
