6229ea8e820e84869fd32f1004c73a993298e1f8
Corey Salzano <938411+csalzano@users.noreply.github.com>
2024-05-06 23:11:14 -0400

    Applies the coding standard to couple JavaScript files.

--- js/order-by-post-meta-widget.js
+++ js/order-by-post-meta-widget.js
@@ -1,10 +1,10 @@
-// returns an associative array
+// Returns an associative array.
 function extract_wp_query_args_from_url() {
 	var url = window.location.href;
-	// get just the query parameters
+	// get just the query parameters.
 	if ( -1 === url.indexOf( '?' ) ) {
 		return {}; }
-	// trim any fragment if we find one
+	// trim any fragment if we find one.
 	if ( -1 !== url.indexOf( '#' ) ) {
 		url = url.substring( 0, url.indexOf( '#' ) );
 	}
@@ -17,7 +17,7 @@ function extract_wp_query_args_from_url() {
 
 
 function html_form_launch( url, method, params ) {
-	// build a form and submit it, found @ https://stackoverflow.com/a/133997/338432
+	// build a form and submit it, found @ https://stackoverflow.com/a/133997/338432.
 	var form = document.createElement( 'form' );
 	form.setAttribute( 'method', method );
 	if ( '' !== url ) {
@@ -38,10 +38,10 @@ function html_form_launch( url, method, params ) {
 }
 
 function order_by_post_meta( key ) {
-	// Submit to the same page with an orderby param
+	// Submit to the same page with an orderby param.
 	var params        = extract_wp_query_args_from_url();
 	params['orderby'] = key;
-	// sorting on same field? reverse the sort order
+	// sorting on same field? reverse the sort order.
 	params['order'] = ( ( undefined !== params['orderby'] && key == params['orderby'] ) && ( 'ASC' == params['order'] || '' === params['order'] ) ? 'DESC' : 'ASC' );
 	html_form_launch( '', 'GET', params );
 }
@@ -49,7 +49,8 @@ function order_by_post_meta( key ) {
 function query_string_to_associative_array( str ) {
 	var arr    = str.replace( '?', '' ).split( '&' );
 	var params = {};
-	for ( var q = 0; q < arr.length; q++ ) {
+	var count  = arr.length;
+	for ( var q = 0; q < count; q++ ) {
 		var pieces        = arr[q].split( '=' );
 		params[pieces[0]] = pieces[1];
 	}
--- js/page-settings.js
+++ js/page-settings.js
@@ -1,35 +1,35 @@
-(function($) {
+(function ($) {
 	jQuery( document ).ready(
-		function() {
-			// add handler on the operator dropdowns so we can toggle the value text box
+		function () {
+			// add handler on the operator dropdowns so we can toggle the value text box.
 			$( 'select.operator' ).on( 'change', toggle_value_input );
-			// toggle the textboxes right now when the page is ready
+			// toggle the textboxes right now when the page is ready.
 			$( 'select.operator' ).each( toggle_value_input );
 
 			// Add a handler when the filter field is changed. We might have to enable or disable other inputs.
 			$( 'select.filter-key' ).on( 'change', toggle_filter_inputs ).each( toggle_filter_inputs );
 
-			// bind this button to add a new row to the table of additional listings pages
+			// bind this button to add a new row to the table of additional listings pages.
 			$( 'button#add_additional_listings_page' ).on(
 				'click',
-				function(){
+				function () {
 					var row_html = $( 'table.additional_listings_pages tbody tr:last' )[0].outerHTML;
 					$( 'table.additional_listings_pages tbody' ).append( repeat_row( row_html ) );
-					// rebind the operator selects
+					// rebind the operator selects.
 					$( 'select.operator' ).off( 'change' );
 					$( 'select.operator' ).on( 'change', toggle_value_input );
 					return false;
 				}
 			);
 
-			// bind the delete buttons
+			// bind the delete buttons.
 			$( 'button.delete-button' ).on( 'click', delete_listings_page );
 		}
 	);
 
 	function delete_listings_page()
 	{
-		// id contains the iterator
+		// id contains the iterator.
 		var id       = $( this ).attr( 'id' );
 		var pieces   = id.split( '_' );
 		var iterator = pieces[pieces.length - 1];
@@ -38,26 +38,26 @@
 
 	function repeat_row( tr_html )
 	{
-		// find the iterator number
+		// find the iterator number.
 		let re        = new RegExp( 'additional_listings_pages_slug_([0-9]+)' );
 		var match_arr = re.exec( tr_html );
 		var iterator  = parseInt( match_arr[match_arr.length - 1] );
 
-		// replace it everywhere
+		// replace it everywhere.
 		re      = new RegExp( '_' + iterator, 'g' );
 		tr_html = tr_html.replace( re, '_' + (iterator + 1) );
 
 		re      = new RegExp( '\\[' + iterator + '\\]' , 'g' );
 		tr_html = tr_html.replace( re, '[' + (iterator + 1) + ']' );
 
-		// reset control values
+		// reset control values.
 		re      = new RegExp( ' selected="selected"' );
 		tr_html = tr_html.replace( re, '' );
 
 		re      = new RegExp( 'value="[^"]+"' );
 		tr_html = tr_html.replace( re, 'value=""' );
 
-		// remove the view link, this page doesn't exist until saved
+		// remove the view link, this page doesn't exist until saved.
 		var mset = jQuery( tr_html );
 		mset.find( 'a.button.action,button.delete-button' ).remove();
 		return mset[0].outerHTML;
@@ -65,23 +65,24 @@
 
 	function toggle_value_input()
 	{
-		// toggle a text box that is not relevant if exists or does not exist is the operator
+		// toggle a text box that is not relevant if exists or does not exist is the operator.
 		var val         = $( this ).val();
 		var is_disabled = ( 'exists' == val || 'does_not_exist' == val );
-		var id          = $( this ).attr( 'id' ); // looks like additional_listings_pages_operator_0
+		var id          = $( this ).attr( 'id' ); // looks like additional_listings_pages_operator_0.
 		var el          = $( '#' + id.replace( 'operator', 'value' ) );
-		if ( is_disabled ) {
+		if (is_disabled ) {
 			el.val( '' );
 		}
 		el.attr( 'disabled', is_disabled );
 	}
 
-	function toggle_filter_inputs() {
+	function toggle_filter_inputs()
+	{
 		var key = $( this ).val();
 		// Maybe disable operator and value.
-		var id = $( this ).attr( 'id' ); // looks like additional_listings_pages_key_2
-		// additional_listings_pages_operator_2
-		// additional_listings_pages_value_2
+		var id = $( this ).attr( 'id' ); // looks like additional_listings_pages_key_2.
+		// additional_listings_pages_operator_2.
+		// additional_listings_pages_value_2.
 		$( '#' + id.replace( '_key_', '_operator_' ) + ',#' + id.replace( '_key_', '_value_' ) ).attr( 'disabled', '' === key );
 	}
 })( jQuery );
