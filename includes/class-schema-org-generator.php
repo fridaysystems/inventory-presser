4341bdf4d8b6d6d06f1ed8a05b4d2c3c17505600
Corey Salzano <938411+csalzano@users.noreply.github.com>
2024-08-02 15:06:21 -0400

    Changes loose comparisons to strict ones. Adds comments and other changes in the schema.org class to satisfy the WP coding standard.

--- includes/class-schema-org-generator.php
+++ includes/class-schema-org-generator.php
@@ -1,21 +1,36 @@
 <?php
-defined( 'ABSPATH' ) || exit;
-
 /**
  * Inventory_Presser_Schema_Org_Generator
  *
  * This class creates schema.org structured data and includes it in the JSON-LD
  * format on vehicle single pages.
  *
+ * @package inventory-presser
  * @see   https://schema.org/Vehicle
  * @since 12.0.0
  */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Inventory_Presser_Schema_Org_Generator
+ */
 class Inventory_Presser_Schema_Org_Generator {
 
+	/**
+	 * Adds hooks that power the feature.
+	 *
+	 * @return void
+	 */
 	public function add_hooks() {
 		add_action( 'wp_body_open', array( $this, 'include_json_ld' ) );
 	}
 
+	/**
+	 * Decides whether to output schema.org data and outputs the data.
+	 *
+	 * @return void
+	 */
 	public function include_json_ld() {
 		if ( ! is_singular( INVP::POST_TYPE ) ) {
 			return;
@@ -30,15 +45,13 @@ class Inventory_Presser_Schema_Org_Generator {
 	}
 
 	/**
-	 * schema_org_drive_type
-	 *
 	 * Translates our drive type term name into a schema.org vehicle drive
 	 * type value.
 	 *
 	 * @param  string $drive_type A drive type term name like "Front Wheel Drive"
 	 * @return string|null A schema.org vehicle drive type string like "FrontWheelDriveConfiguration"
 	 */
-	function schema_org_drive_type( $drive_type ) {
+	protected function schema_org_drive_type( $drive_type ) {
 		switch ( $drive_type ) {
 			case 'Front Wheel Drive w/4x4':
 			case 'Rear Wheel Drive w/4x4':
@@ -58,13 +71,12 @@ class Inventory_Presser_Schema_Org_Generator {
 	}
 
 	/**
-	 * schema_org_json_ld
-	 *
 	 * Returns Schema.org markup for this Vehicle as a JSON-LD code block
 	 *
+	 * @param  int $post_ID The current post ID.
 	 * @return string Schema.org JSON script element
 	 */
-	function schema_org_json_ld( $post_ID ) {
+	protected function schema_org_json_ld( $post_ID ) {
 		$obj = array(
 			'@context' => 'https://schema.org/',
 			'@type'    => 'Vehicle',
@@ -73,7 +85,7 @@ class Inventory_Presser_Schema_Org_Generator {
 		$obj['name'] = get_the_title( $post_ID );
 
 		$make = invp_get_the_make( $post_ID );
-		if ( '' != $make ) {
+		if ( '' !== $make ) {
 			$obj['brand'] = array(
 				'@type' => 'Thing',
 				'name'  => $make,
@@ -81,23 +93,23 @@ class Inventory_Presser_Schema_Org_Generator {
 		}
 
 		$vin = invp_get_the_VIN( $post_ID );
-		if ( '' != $vin ) {
+		if ( '' !== $vin ) {
 			$obj['vehicleIdentificationNumber'] = $vin;
 		}
 
 		$year = invp_get_the_year( $post_ID );
-		if ( 0 != $year ) {
+		if ( 0 !== $year ) {
 			$obj['vehicleModelDate'] = $year;
 		}
 
-		// if the image does not end with 'no-photo.png'
-		if ( 'no-photo.png' != substr( invp_get_the_photo_url( $post_ID ), 12 ) ) {
+		// if the image does not end with 'no-photo.png'.
+		if ( 'no-photo.svg' !== substr( invp_get_the_photo_url( $post_ID ), 12 ) ) {
 			$obj['image'] = invp_get_the_photo_url( $post_ID );
 		}
 
 		$odometer = invp_get_the_odometer( '', $post_ID );
-		if ( '' != $odometer ) {
-			// Extract just digits from the odometer value
+		if ( '' !== $odometer ) {
+			// Extract just digits from the odometer value.
 			$odometer_digits            = abs( (int) filter_var( $odometer, FILTER_SANITIZE_NUMBER_INT ) );
 			$obj['mileageFromOdometer'] = array(
 				'@type'    => 'QuantitativeValue',
@@ -106,25 +118,25 @@ class Inventory_Presser_Schema_Org_Generator {
 			);
 		}
 
-		if ( '' != invp_get_the_engine( $post_ID ) || '' != invp_get_the_fuel( $post_ID ) ) {
+		if ( '' !== invp_get_the_engine( $post_ID ) || '' !== invp_get_the_fuel( $post_ID ) ) {
 			$obj['vehicleEngine'] = array();
-			if ( '' != invp_get_the_engine( $post_ID ) ) {
+			if ( '' !== invp_get_the_engine( $post_ID ) ) {
 				$obj['vehicleEngine']['engineType'] = invp_get_the_engine( $post_ID );
 			}
-			if ( '' != invp_get_the_fuel( $post_ID ) ) {
+			if ( '' !== invp_get_the_fuel( $post_ID ) ) {
 				$obj['vehicleEngine']['fuelType'] = invp_get_the_fuel( $post_ID );
 			}
 		}
 
-		if ( '' != invp_get_the_body_style( $post_ID ) ) {
+		if ( '' !== invp_get_the_body_style( $post_ID ) ) {
 			$obj['bodyType'] = invp_get_the_body_style( $post_ID );
 		}
 
-		if ( '' != invp_get_the_color( $post_ID ) ) {
+		if ( '' !== invp_get_the_color( $post_ID ) ) {
 			$obj['color'] = invp_get_the_color( $post_ID );
 		}
 
-		if ( '' != invp_get_the_interior_color( $post_ID ) ) {
+		if ( '' !== invp_get_the_interior_color( $post_ID ) ) {
 			$obj['vehicleInteriorColor'] = invp_get_the_interior_color( $post_ID );
 		}
 
@@ -137,7 +149,7 @@ class Inventory_Presser_Schema_Org_Generator {
 			$obj['driveWheelConfiguration'] = $schema_drive_type;
 		}
 
-		if ( '' != invp_get_the_transmission( $post_ID ) ) {
+		if ( '' !== invp_get_the_transmission( $post_ID ) ) {
 			$obj['vehicleTransmission'] = invp_get_the_transmission( $post_ID );
 		}
 
