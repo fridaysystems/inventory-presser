da56d6ba11d8158ff60ea48516b9a3f3b895bc6e
Corey Salzano <938411+csalzano@users.noreply.github.com>
2024-11-04 19:20:03 -0500

    Enhances the YouTube Video ID block to render the video embed in the editor when the value entered is a valid video ID. The iframe embed HTML is also stashed in a new meta field inventory_presser_youtube_embed. Thanks be to @ryanwelcher for reviewing my code, writing, and explaining a solution.

--- build/blocks/youtube/index.asset.php
+++ build/blocks/youtube/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '23fc00f377125a430607');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '1ce8b19211568d06e92d');
--- src/blocks/youtube/edit.js
+++ src/blocks/youtube/edit.js
@@ -1,49 +1,85 @@
-/**
- * WordPress dependencies
- */
 import { __ } from '@wordpress/i18n';
 import { TextControl } from '@wordpress/components';
 import { useEntityProp } from '@wordpress/core-data';
-import { useSelect } from '@wordpress/data';
 import { useBlockProps } from '@wordpress/block-editor';
+import { useEffect } from '@wordpress/element';
 
-export default function Edit( { isSelected } ) {
+/**
+ * In order for a block to render HTML stored in a meta value, we need this
+ * RawHTML function.
+ * @link https://wordpress.stackexchange.com/a/409328/13090
+ */
+const htmlToElem = ( html ) => wp.element.RawHTML( { children: html } );
 
-	const postType = useSelect(
-		( select ) => select( 'core/editor' ).getCurrentPostType(),
-		[]
-	);
+export default function Edit( { isSelected, context } ) {
+	const { postType } = context;
 	const [ meta, setMeta ] = useEntityProp( 'postType', postType, 'meta' );
 	const blockProps = useBlockProps();
+	// Two meta fields hold YouTube video ID and the iframe HTML.
+	const metaValue      = meta[ invp_blocks.meta_prefix + 'youtube' ];
+	const metaValueEmbed = meta[ invp_blocks.meta_prefix + 'youtube_embed' ];
 
-	/**
-	 * Use isSelected to render the lost focus output that mimics the front-end
-	 *
-	 * @link https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#isselected
-	 */
-	if ( ! isSelected ) {
-		wp.apiRequest({
+	// Handle metaValue changes.
+	useEffect( () => {
+		// Avoid unnecessary API requests.
+		if ( '' === metaValue || 11 > metaValue.length ) {
+			return;
+		}
+		// Get iframe HTML from the YouTube API.
+		wp.apiRequest( {
 			url: wp.media.view.settings.oEmbedProxyUrl,
 			data: {
-				url: 'https://www.youtube.com/watch?v=' + meta[ invp_blocks.meta_prefix + 'youtube' ],
+				url: 'https://www.youtube.com/watch?v=' + metaValue,
 			},
 			type: 'GET',
 			dataType: 'json',
-			context: this
-		})
-		.done( function( response ) {
-			document.getElementById( blockProps.id + '-oembed' ).innerHTML = response.html;
+			context: this,
+		} )
+		.done( youTubeApiCallbackSuccess ) // The new "success()".
+		.fail( ( response ) => { // The new "error()".
+			console.log( 'YouTube API error', response.responseJSON );
 		} );
-		return ( <div { ...blockProps }> <div id={ blockProps.id + '-oembed' } > { meta[ invp_blocks.meta_prefix + 'youtube' ] }</div></div> );
+	}, [ metaValue ] );
+
+	const youTubeApiCallbackSuccess = ( response ) => {
+		// Save the iframe HTML in meta.
+		setMeta( { ...meta, [invp_blocks.meta_prefix + 'youtube_embed']: response.html } );
+		// Put the iframe HTML in the block.
+		const container = document.getElementById( blockProps.id + '-oembed' );
+		if ( container ) {
+			container.innerHTML = response.html;
+		}
 	}
 
 	return (
 		<div { ...blockProps }>
-			<TextControl
-				label    = { __( 'YouTube Video ID', 'inventory-presser' ) }
-				value    = { meta[ invp_blocks.meta_prefix + 'youtube' ] }
-				onChange = {(newValue) => setMeta( { ...meta, [invp_blocks.meta_prefix + 'youtube']: newValue } )}
-			/>
+			{
+				/**
+				 * Use isSelected to render the block differently if the user is
+				 * editing it. Also, don't hide the TextControl if the iframe
+				 * HTML is empty. Wait until the user inputs an ID and the
+				 * YouTube API responds with HTML.
+				 *
+				 * @link https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#isselected
+				 */
+				isSelected || '' === metaValueEmbed ? (
+					<TextControl
+						label={ __( 'YouTube Video ID', 'inventory-presser' ) }
+						value={ metaValue }
+						onChange={ ( videoId ) =>
+							setMeta( {
+								...meta,
+								[ invp_blocks.meta_prefix + 'youtube' ]: videoId,
+								[ invp_blocks.meta_prefix + 'youtube_embed' ]: ''
+							} )
+						}
+					/>
+				) : (
+					<div id={ blockProps.id + '-oembed' }>
+						{ htmlToElem( metaValueEmbed ) }
+					</div>
+				)
+			}
 		</div>
 	);
 }
