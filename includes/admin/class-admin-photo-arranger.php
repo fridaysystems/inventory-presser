859ef24d2d37e79c923f0815f1848f8c6e2cb82d
Corey Salzano <938411+csalzano@users.noreply.github.com>
2025-10-31 12:27:39 -0400

    Deletes commented out code

--- includes/admin/class-admin-photo-arranger.php
+++ includes/admin/class-admin-photo-arranger.php
@@ -268,20 +268,6 @@ class Inventory_Presser_Admin_Photo_Arranger {
 		);
 		$post->post_content = serialize_blocks( $blocks );
 		$this->safe_update_post( $post );
-
-			// Add all this vehicle's photos.
-			// $posts = get_children(
-			// array(
-			// 'meta_key'    => apply_filters( 'invp_prefix_meta_key', 'photo_number' ),
-			// 'order'       => 'ASC',
-			// 'orderby'     => 'meta_value_num',
-			// 'post_parent' => $post->ID,
-			// 'post_type'   => 'attachment',
-			// )
-			// );
-			// foreach ( $posts as $post ) {
-			// $this->add_attachment_to_gallery( $post->ID, $post->post_parent );
-			// }
 	}
 
 	/**
