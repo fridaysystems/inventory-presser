e5f26a97a10500f4966e265fe0bd48d63046482d
Corey Salzano <938411+csalzano@users.noreply.github.com>
2025-11-13 12:12:23 -0500

    Changes line endings and whitespace. Updates built .js

--- build/blocks/youtube/index.asset.php
+++ build/blocks/youtube/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '1ce8b19211568d06e92d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n'), 'version' => '542e2ccf62c23c918d17');
